4383f28450a8e8fb817403a9820fde5691c52157
Hetul <hetul@Hetuls-MacBook-Pro.local>
2020-07-08 01:10:03 +0530

    import issue resolve

--- core/backbone.py
+++ core/backbone.py
@@ -19,38 +19,38 @@ def darknet53(input_data, trainable):
 
     with tf.variable_scope('darknet'):
 
-        input_data = convolutional(input_data, filters_shape=(3, 3,  3,  12), trainable=trainable, name='conv0')
-        input_data = convolutional(input_data, filters_shape=(3, 3, 12,  24),
+        input_data = common.convolutional(input_data, filters_shape=(3, 3,  3,  12), trainable=trainable, name='conv0')
+        input_data = common.convolutional(input_data, filters_shape=(3, 3, 12,  24),
                                           trainable=trainable, name='conv1', downsample=True)
 
         for i in range(1):
-            input_data = residual_block(input_data,  24,  12, 24, trainable=trainable, name='residual%d' %(i+0))
+            input_data = common.residual_block(input_data,  24,  12, 24, trainable=trainable, name='residual%d' %(i+0))
 
-        input_data = convolutional(input_data, filters_shape=(3, 3,  24, 48),
+        input_data = common.convolutional(input_data, filters_shape=(3, 3,  24, 48),
                                           trainable=trainable, name='conv4', downsample=True)
 
         for i in range(2):
-            input_data = residual_block(input_data, 48,  24, 48, trainable=trainable, name='residual%d' %(i+1))
+            input_data = common.residual_block(input_data, 48,  24, 48, trainable=trainable, name='residual%d' %(i+1))
 
-        input_data = convolutional(input_data, filters_shape=(3, 3, 48, 64),
+        input_data = common.convolutional(input_data, filters_shape=(3, 3, 48, 64),
                                           trainable=trainable, name='conv9', downsample=True)
 
         for i in range(2):
-            input_data = residual_block(input_data, 64, 32, 64, trainable=trainable, name='residual%d' %(i+3))
+            input_data = common.residual_block(input_data, 64, 32, 64, trainable=trainable, name='residual%d' %(i+3))
 
         route_1 = input_data
-        input_data = convolutional(input_data, filters_shape=(3, 3, 64, 64),
+        input_data = common.convolutional(input_data, filters_shape=(3, 3, 64, 64),
                                           trainable=trainable, name='conv26', downsample=True)
 
         for i in range(2):
-            input_data = residual_block(input_data, 64, 32, 64, trainable=trainable, name='residual%d' %(i+11))
+            input_data = common.residual_block(input_data, 64, 32, 64, trainable=trainable, name='residual%d' %(i+11))
 
         route_2 = input_data
-        input_data = convolutional(input_data, filters_shape=(3, 3, 64, 128),
+        input_data = common.convolutional(input_data, filters_shape=(3, 3, 64, 128),
                                           trainable=trainable, name='conv43', downsample=True)
 
         for i in range(2):
-            input_data = residual_block(input_data, 128, 64, 128, trainable=trainable, name='residual%d' %(i+19))#256
+            input_data = common.residual_block(input_data, 128, 64, 128, trainable=trainable, name='residual%d' %(i+19))#256
 
         return route_1, route_2, input_data
 
